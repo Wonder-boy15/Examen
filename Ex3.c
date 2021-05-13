f00ae741ae0a314b544f46b45571bdd8a6eb7fe4
OsMaravilla15 <82550242+Wonder-boy15@users.noreply.github.com>
2021-05-12 21:36:13 -0500

    Add files via upload

--- Ex3.c
+++ Ex3.c
@@ -15,7 +15,7 @@ int main()
    scanf ( "%f", &Fresa);
    printf ( "\n\n ingrese el peso de la fruta seleccionada");
    scanf ( "%f", &5kg);
-  precio= &5kg * &13
+  precio= &5kg * &$13
   printf ("\n\n El precio total es %f", precio );
 
    return  0 ;
